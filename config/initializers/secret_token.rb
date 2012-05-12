0e7b0b7547cb07d6dd03767fd7f025396f9f631c
Carolina Gracia <carogracia@hotmail.com>
2012-05-11 23:11:27 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Segurosenlinea::Application.config.secret_token = '6a236e9868064516b0a06dedf492b38f3b3fa122361d2f5704a1691f4bdf3cfde89219c8df703db6b8379f4d5cc5aed6a23b29a92765683a8e9e2bfa8f7d04c7'
